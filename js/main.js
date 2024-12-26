c902e69aac7d5649a975121f390e986897192c1c
peterkkhan <juna08@naver.com>
2024-12-26 14:49:45 +0900

    프로젝트 시작

---
+++ js/main.js
@@ -0,0 +1,52 @@
+//장바구니!
+
+const basketStarterEl = document.querySelector('header .basket-starter')
+const basketEl = basketStarterEl.querySelector('.basket')
+
+basketStarterEl.addEventListener('click', function (event) {
+  event.stopPropagation()
+  if(basketEl.classList.contains('show')) {
+    hideBasket()
+  } else {
+    showBasket()
+  }
+})
+
+basketEl.addEventListener('click', function(event) {
+  event.stopPropagation()
+})
+
+window.addEventListener('click', function() {
+  hideBasket()
+})
+
+function showBasket () {
+  basketEl.classList.add('show')
+}
+
+function hideBasket () {
+  basketEl.classList.remove('show')
+}
+
+// 검색!
+
+const headerEl = document.querySelector('header')
+const searchWrapEl = headerEl.querySelector('.search-wrap')
+const searchStarterEl = headerEl.querySelector('.search-starter')
+const searchCloserEl =  searchWrapEl.querySelector('.search-closer')
+const searchshadowEl =  searchWrapEl.querySelector('.shadow')
+
+
+
+searchStarterEl.addEventListener('click', showSearch)
+searchCloserEl.addEventListener('click', hideSearch)
+searchshadowEl.addEventListener('click', hideSearch)
+
+function showSearch() {
+  headerEl.classList.add('searching')
+  document.documentElement.classList.add('fixed')
+}
+function hideSearch() {
+  headerEl.classList.remove('searching')
+  document.documentElement.classList.remove('fixed')
+}
