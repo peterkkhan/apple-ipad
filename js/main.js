b62eb71e2305b7561d822fee9b54034fabde9f43
peterkkhan <juna08@naver.com>
2024-12-31 21:06:47 +0900

    모바일 검색 버그 수정

--- js/main.js
+++ js/main.js
@@ -45,7 +45,10 @@ const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
 
 
 searchStarterEl.addEventListener('click', showSearch)
-searchCloserEl.addEventListener('click', hideSearch)
+searchCloserEl.addEventListener('click', function (event) {
+  event.stopPropagation()
+  hideSearch()
+})
 searchshadowEl.addEventListener('click', hideSearch)
 
 function showSearch() {
@@ -88,6 +91,7 @@ const menuStarterEl = document.querySelector('header .menu-starter')
 menuStarterEl.addEventListener('click', function () {
   if (headerEl.classList.contains('menuing')) {
     headerEl.classList.remove('menuing')
+    searchInputEl.value = ''
     playScroll()
   } else {
     headerEl.classList.add('menuing')
@@ -95,7 +99,25 @@ menuStarterEl.addEventListener('click', function () {
   }
 })
 
+//헤더 검색!
+const searchTextFieldEl = document.querySelector('header .textfield')
+const searchCancelEl = document.querySelector('header .search-canceler')
+searchTextFieldEl.addEventListener('click', function () {
+  headerEl.classList.add('searching--mobile')
+  searchInputEl.focus()
+})
+searchCancelEl.addEventListener('click', function () {
+  headerEl.classList.remove('searching--mobile')
+})
 
+//
+window.addEventListener('resize', function () {
+  if (window.innerWidth <= 740) {
+    headerEl.classList.remove('searching')
+  } else {
+    headerEl.classList.remove('searching--mobile')
+  }
+})
 
 
 
